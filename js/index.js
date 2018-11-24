df49bc374d3787670e8a01fa4e7dc30b720bfa8e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-24 20:16:55 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+function toggleColor(target){
+  if(target.style.backgroundColor === "red"){
+    target.style.backgroundColor = "white";
+  } else {
+    target.style.backgroundColor = "red";
+  }
+}
+
+document.addEventListener('click', function(event){
+  if(event.target.tagName === "DIV"){
+    toggleColor(event.target);
+  }
+});
